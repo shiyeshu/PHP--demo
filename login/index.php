d83d45b59f0a788a5b67fda045cdf95e56a58225
sshuo <924829024@qq.com>
2018-05-22 18:16:20 +0800

    Update index.php

--- login/index.php
+++ login/index.php
@@ -1,11 +1,14 @@
 
 <?php
-@$password=$_POST['password'];
-if ($_POST["password"] = $password) {
+$password="admin";
+if ($_POST["password"] == $password) {
 	session_start();
 	$_SESSION['username'] = $password;
-	header("Location: admin.php");
+	header("Location:admin.php");
 } else {
 	echo "密码错误或未输入";
 }
-?>
+
+
+
+  ?>
